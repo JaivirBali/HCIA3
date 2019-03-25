0c4d662a6f7012bb652aa6a2b1b2e7eaad97e80d
PaoloGio <gotopg@myumanitoba.ca>
2019-03-24 20:28:11 -0500

    buttons added.

--- app/src/main/java/com/example/hci_a3/MainActivity.java
+++ app/src/main/java/com/example/hci_a3/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.hci_a3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_buttons);
+
+        TextView cmdPrompt = findViewById(R.id.cmdTxtview);
+
+        Button Abutton = findViewById(R.id.aBtn);
+        Button Bbutton = findViewById(R.id.bBtn);
+        Button Cbutton = findViewById(R.id.cBtn);
+        Button Dbutton = findViewById(R.id.dBtn);
+        Button Ebutton = findViewById(R.id.eBtn);
+        Button Fbutton = findViewById(R.id.fBtn);
+        Button Gbutton = findViewById(R.id.gBtn);
+
+        Button button1 = findViewById(R.id.Btn1);
+        Button button2 = findViewById(R.id.Btn2);
+        Button button3 = findViewById(R.id.Btn3);
+        Button button4 = findViewById(R.id.Btn4);
+        Button button5 = findViewById(R.id.Btn5);
+        Button button6 = findViewById(R.id.Btn6);
+        Button button7 = findViewById(R.id.Btn7);
+        Button button8 = findViewById(R.id.Btn8);
+        Button button9 = findViewById(R.id.Btn9);
+        Button button10 = findViewById(R.id.Btn10);
+        Button button11 = findViewById(R.id.Btn11);
+        Button button12 = findViewById(R.id.Btn12);
+        Button button13 = findViewById(R.id.Btn13);
+        Button button14 = findViewById(R.id.Btn14);
+        Button button15 = findViewById(R.id.Btn15);
+        Button button16 = findViewById(R.id.Btn16);
+        Button button17 = findViewById(R.id.Btn17);
+        Button button18 = findViewById(R.id.Btn18);
+        Button button19 = findViewById(R.id.Btn19);
+        Button button20 = findViewById(R.id.Btn20);
+        Button button21 = findViewById(R.id.Btn21);
+        Button button22 = findViewById(R.id.Btn22);
+
     }
 }

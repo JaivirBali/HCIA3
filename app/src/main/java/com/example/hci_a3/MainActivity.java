e13070e3967e0a4825b8ac1f0ae9e2b43973b9c1
Jay Bali <jaivirbali@live.com>
2019-03-28 18:47:04 -0500

    -Changed buttons to 3 different sizes

--- app/src/main/java/com/example/hci_a3/MainActivity.java
+++ app/src/main/java/com/example/hci_a3/MainActivity.java
@@ -23,13 +23,12 @@ public class MainActivity extends AppCompatActivity {
 
         TextView cmdPrompt = findViewById(R.id.cmdTxtview);
 
-        Button Abutton = findViewById(R.id.aBtn);
-        Button Bbutton = findViewById(R.id.bBtn);
-        Button Cbutton = findViewById(R.id.cBtn);
-        Button Dbutton = findViewById(R.id.dBtn);
-        Button Ebutton = findViewById(R.id.eBtn);
-        Button Fbutton = findViewById(R.id.fBtn);
-        Button Gbutton = findViewById(R.id.gBtn);
+        Button L1button = findViewById(R.id.L1btn);
+        Button L2button = findViewById(R.id.L2btn);
+        Button L3button = findViewById(R.id.L3btn);
+        Button L4button = findViewById(R.id.L4btn);
+        Button L5button = findViewById(R.id.L5btn);
+        Button L6button = findViewById(R.id.L6btn);
 
         Button button1 = findViewById(R.id.Btn1);
         Button button2 = findViewById(R.id.Btn2);
@@ -39,9 +38,6 @@ public class MainActivity extends AppCompatActivity {
         Button button6 = findViewById(R.id.Btn6);
         Button button7 = findViewById(R.id.Btn7);
         Button button8 = findViewById(R.id.Btn8);
-        Button button9 = findViewById(R.id.Btn9);
-        Button button10 = findViewById(R.id.Btn10);
-        Button button11 = findViewById(R.id.Btn11);
         Button button12 = findViewById(R.id.Btn12);
         Button button13 = findViewById(R.id.Btn13);
         Button button14 = findViewById(R.id.Btn14);

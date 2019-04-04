affd0483ee8d342a586617c4a11a2d6593eddf41
DerekMartens <33992280+DerekMartens@users.noreply.github.com>
2019-04-03 22:25:24 -0500

    Added seperate tracking for small, medium, and large targets.

--- app/src/main/java/com/example/hci_a3/MainActivity.java
+++ app/src/main/java/com/example/hci_a3/MainActivity.java
@@ -14,9 +14,18 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<Button> buttonsAR = new ArrayList<>();
-    Button currActiveButton = null;
+    private final int NUM_TRIALS = 20;
+    private ArrayList<Button> largeButtonsAR = new ArrayList<>();
+    private ArrayList<Button> mediumButtonsAR = new ArrayList<>();
+    private ArrayList<Button> smallButtonsAR = new ArrayList<>();
+    private Button currActiveButton = null;
+    private int smallTrials = 0;
+    private int mediumTrials = 0;
+    private int largeTrials = 0;
 
+    public int getSmallTrials(){return smallTrials;}
+    public int getMediumTrials(){return mediumTrials;}
+    public int getLargeTrials(){return largeTrials;}
     public Button getCurrActiveButton() {
         return currActiveButton;
     }
@@ -25,7 +34,24 @@ public class MainActivity extends AppCompatActivity {
         currActiveButton.getBackground().setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
     }
     public void setRandomActiveButton(){
-        Button b = buttonsAR.get((int)(Math.random()*buttonsAR.size()));
+        //pick a random size group
+
+
+        //if that size group has less trials than NUM_TRIALS, find a random button in that group
+
+        //else pick a different size group
+
+    }
+    public void setRandomSmallButton(){
+        Button b = smallButtonsAR.get((int)(Math.random()*smallButtonsAR.size()));
+        setCurrActiveButton(b);
+    }
+    public void setRandomMediumButton(){
+        Button b = mediumButtonsAR.get((int)(Math.random()*mediumButtonsAR.size()));
+        setCurrActiveButton(b);
+    }
+    public void setRandomLargeButton(){
+        Button b = largeButtonsAR.get((int)(Math.random()*largeButtonsAR.size()));
         setCurrActiveButton(b);
     }
 
@@ -62,55 +88,55 @@ public class MainActivity extends AppCompatActivity {
         TextView cmdPrompt = findViewById(R.id.cmdTxtview);
 
         Button L1button = findViewById(R.id.L1btn);
-        buttonsAR.add(L1button);
+        largeButtonsAR.add(L1button);
         Button L2button = findViewById(R.id.L2btn);
-        buttonsAR.add(L2button);
+        largeButtonsAR.add(L2button);
         Button L3button = findViewById(R.id.L3btn);
-        buttonsAR.add(L3button);
+        largeButtonsAR.add(L3button);
         Button L4button = findViewById(R.id.L4btn);
-        buttonsAR.add(L4button);
+        largeButtonsAR.add(L4button);
         Button L5button = findViewById(R.id.L5btn);
-        buttonsAR.add(L5button);
+        largeButtonsAR.add(L5button);
         Button L6button = findViewById(R.id.L6btn);
-        buttonsAR.add(L6button);
+        largeButtonsAR.add(L6button);
         Button button1 = findViewById(R.id.Btn1);
-        buttonsAR.add(button1);
+        mediumButtonsAR.add(button1);
         Button button2 = findViewById(R.id.Btn2);
-        buttonsAR.add(button2);
+        mediumButtonsAR.add(button2);
         Button button3 = findViewById(R.id.Btn3);
-        buttonsAR.add(button3);
+        mediumButtonsAR.add(button3);
         Button button4 = findViewById(R.id.Btn4);
-        buttonsAR.add(button4);
+        mediumButtonsAR.add(button4);
         Button button5 = findViewById(R.id.Btn5);
-        buttonsAR.add(button5);
+        mediumButtonsAR.add(button5);
         Button button6 = findViewById(R.id.Btn6);
-        buttonsAR.add(button6);
+        mediumButtonsAR.add(button6);
         Button button7 = findViewById(R.id.Btn7);
-        buttonsAR.add(button7);
+        mediumButtonsAR.add(button7);
         Button button8 = findViewById(R.id.Btn8);
-        buttonsAR.add(button8);
+        mediumButtonsAR.add(button8);
         Button button12 = findViewById(R.id.Btn12);
-        buttonsAR.add(button12);
+        smallButtonsAR.add(button12);
         Button button13 = findViewById(R.id.Btn13);
-        buttonsAR.add(button13);
+        smallButtonsAR.add(button13);
         Button button14 = findViewById(R.id.Btn14);
-        buttonsAR.add(button14);
+        smallButtonsAR.add(button14);
         Button button15 = findViewById(R.id.Btn15);
-        buttonsAR.add(button15);
+        smallButtonsAR.add(button15);
         Button button16 = findViewById(R.id.Btn16);
-        buttonsAR.add(button16);
+        smallButtonsAR.add(button16);
         Button button17 = findViewById(R.id.Btn17);
-        buttonsAR.add(button17);
+        smallButtonsAR.add(button17);
         Button button18 = findViewById(R.id.Btn18);
-        buttonsAR.add(button18);
+        smallButtonsAR.add(button18);
         Button button19 = findViewById(R.id.Btn19);
-        buttonsAR.add(button19);
+        smallButtonsAR.add(button19);
         Button button20 = findViewById(R.id.Btn20);
-        buttonsAR.add(button20);
+        smallButtonsAR.add(button20);
         Button button21 = findViewById(R.id.Btn21);
-        buttonsAR.add(button21);
+        smallButtonsAR.add(button21);
         Button button22 = findViewById(R.id.Btn22);
-        buttonsAR.add(button22);
+        smallButtonsAR.add(button22);
         setRandomActiveButton();
 
     }
